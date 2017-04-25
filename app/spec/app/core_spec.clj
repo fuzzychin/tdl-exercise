26fb4a7c1bd34c8fe08d6628047f810c42d246c6
Adil Khan <adil.khan@outlook.com>
2017-04-24 20:57:16 -0400

    Start the testing of string functions

---
+++ app/spec/app/core_spec.clj
@@ -0,0 +1,8 @@
+(ns app.core-spec
+  (:require [speclj.core :refer :all]
+            [app.core :refer :all]
+            [clojure.string :refer :all]))
+
+(describe "the joy of strings"
+  (it "FIXME, I fail."
+    (should= true (blank? ""))))
