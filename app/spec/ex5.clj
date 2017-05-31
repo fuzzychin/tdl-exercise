4d116f734517d2a1578367d32d48299c9b619866
Adil Khan <adil.khan@outlook.com>
2017-05-31 14:58:39 -0400

    Triangle Kata ex 5

---
+++ app/spec/ex5.clj
@@ -0,0 +1,11 @@
+(ns app.core-spec
+  (:require [speclj.core :refer :all]
+            [app.core :refer :all]))
+
+
+(describe "===Ex5===")
+
+(describe "Triangle Kata"
+(it "Can identify a isocelese triangle")
+(it "Can identify a equilateral triangle")
+(it "Can identify a scalene triangle"))
