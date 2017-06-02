02dee63cc745acbb25b33e14d70decb80ada7da4
Adil Khan <adil.khan@outlook.com>
2017-06-02 12:01:44 -0400

    adds solution to ex3

--- app/spec/ex3.clj
+++ app/spec/ex3.clj
@@ -23,12 +23,32 @@
     (should= 4 (square-func 2))))
 
 (describe "Branching logic"
-(it "a function with branching logic to determin odd/even" 
-    (defn odd
-      [input]
-      (if (= 1 (mod input 2))
-        true
-        false
-      ))
+  (defn odd
+    [input]
+    (if (= 1 (mod input 2))
+      true
+      false
+    ))
+  (it "a function with branching logic to determin odd/even" 
     (should= true (odd 3))
     (should= false (odd 4))))
+
+(describe "Multi Arity Functions"
+  (defn multi-airty 
+   ([arg1]
+    "one")
+   ([arg1 arg2] 
+    "two")
+   ([arg1 arg2 arg3] 
+    "three")) 
+  (it "dpending on how many arguments are passed a different branch is executed"
+    (should= "one" (multi-airty 1))
+    (should= "two" (multi-airty 1 1))
+    (should= "three" (multi-airty 1 1 1))))
+
+
+(describe "Anonymous functions"
+  (it "I can create an anonymous function to make a square of a num"
+      (should= 4 ((fn [x] (* x x)) 2))))
+
+
