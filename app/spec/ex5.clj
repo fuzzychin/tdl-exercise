bf0872d0eb766fe402d120d5e9de4b1ce3c2dfdd
Adil Khan <adil.khan@outlook.com>
2017-06-05 18:19:14 -0400

    working triangle kata

--- app/spec/ex5.clj
+++ app/spec/ex5.clj
@@ -18,7 +18,9 @@
 
 (defn classify
   [s1 s2 s3]
-   (equi s1 s2 s3) )
+   (if (equi s1 s2 s3) "equi" 
+     (if (scal s1 s2 s3) "scal" 
+       (if (isoc s1 s2 s3) "isoc" )))) 
 
 (describe "Triangle Kata"
 (it "Can identify a isocelese triangle" 
@@ -28,5 +30,15 @@
      (should= true (equi 1 1 1 )))
 
 (it "Can identify a scalene triangle"
-     (should= true (scal 4 5 3))))
+     (should= true (scal 4 5 3)))
+
+(it "can classify a equilateral triangle"
+    (should= "equi" (classify 2 2 2)))
+
+(it "can classify a scalene triangle"
+    (should= "scal" (classify 2 4 5)))
+
+(it "can classify a isocelese triangle"
+    (should= "isoc" (classify 2 2 3)))
+)
 
