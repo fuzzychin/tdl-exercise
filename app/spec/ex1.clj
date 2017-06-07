00cee28708697a83f771bd01f0a4fa09df453f3a
GaryS <gary.springer@kroger.com>
2017-06-07 19:26:56 -0400

    ex1 and ex2

--- app/spec/ex1.clj
+++ app/spec/ex1.clj
@@ -3,5 +3,5 @@
             [app.core :refer :all]))
 
 (describe "a test"
-  (it "passes"
-    (should= 1 1)))
+  (it "FIXME, I fail."
+    (should= 0 1)))
--- app/spec/ex2.clj
+++ app/spec/ex2.clj
@@ -8,11 +8,11 @@
 
 (describe "simple arithmetic"
  (it "2+2 is 4"
-   (should= 4 (+ 2 1)))
+   (should= 4 (+ 2 2)))
 
  (it "2x4 is 8"
-   (should= 8 (* 2 3)))
+   (should= 8 (* 2 4)))
 
  (it "8/4 is 2"
-   (should= 2 ("put expression here"))))
+   (should= 2 (/ 8 4))))
 
